0626b6047dfff1062a1329b57ede4e83885b07eb
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-10 12:18:26 +0100

    feat(model) Add SpamReport viewset

--- src/spam_reports_project/spam_reports_api/views.py
+++ src/spam_reports_project/spam_reports_api/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from . import serializers
+from . import models
+from . import permissions
+
 # Create your views here.
+
+class SpamReportsItemViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading, updating and deleting spam report items."""
+
+    serializer_class = serializers.SpamReportItemSerializer
+    queryset = models.SpamReportsItem.objects.all()
+    permission_classes = (permissions.UpdatePermissions, IsAuthenticatedOrReadOnly)
