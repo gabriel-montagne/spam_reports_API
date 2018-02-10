e48aac79c309216c94697813694579dd5e242ea6
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-10 22:10:16 +0100

    feat(view) Add JSON parser and customize update(put)

--- src/spam_reports_project/spam_reports_api/views.py
+++ src/spam_reports_project/spam_reports_api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.parsers import JSONParser
 
 from . import serializers
 from . import models
@@ -20,3 +21,14 @@ class SpamReportsItemViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.SpamReportItemSerializer
     queryset = models.SpamReportsItem.objects.all()
     permission_classes = (permissions.UpdatePermissions, IsAuthenticatedOrReadOnly)
+    parser_classes = (JSONParser,)
+
+
+    def update(self, request, pk=None):
+
+        instance = self.get_object()
+        instance.state = request.data['status']
+        serializer = self.serializer_class(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
